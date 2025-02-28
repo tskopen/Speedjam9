4a8fd0f4542cc50f0da99cf4d79471244279179f
tskopen <tskopen@gmail.com>
2025-02-28 18:24:00 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+/*
+ * Main gameloop of speedjam9
+ *
+ * Name:    Thor Kopenkoskey
+ * Date:   Feburary 22, 2025
+ */
+
+ #include <iostream>
+ #include <SFML/Window.hpp>
+ #include <SFML/Graphics.hpp>
+
+ using namespace std;
+
+ /*
+  * main - Main gameloop of speedjam9
+  *
+  * Return:  status
+  */
+ 
+ int main()
+ {
+   sf::Window window;
+   window.create(sf::VideoMode({800, 600}), "My window");
+ }
+
